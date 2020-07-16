97bd418cefd1e58dca5c8bb5fe3009eb2e6fdd45
Cory Spicer <corys4139@gmail.com>
2020-07-16 03:12:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ['arrays!', 'earth', 'fire', 'water', 'air']
+  array = ['wow', 'earth', 'fire', 'water', 'air']
   element = array.shift
 end
 
