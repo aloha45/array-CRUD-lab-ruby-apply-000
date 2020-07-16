fee2b95c2da9d8798db047070c33f65434de8aee
Cory Spicer <corys4139@gmail.com>
2020-07-16 03:07:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ['fire', 'water', 'earth']
-  array << 'air'
+  array << 'arrays!'
 end
 
 def add_element_to_start_of_array(array, element)
