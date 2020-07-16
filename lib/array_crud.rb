87014ae32c0978dbf5ec79d146fa21de97f8e357
Cory Spicer <corys4139@gmail.com>
2020-07-16 03:16:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array = ['wow', 'earth', 'fire', 'water', 'air']
+  array = ['wow', 'earth', 'fire', 'water', 'air', 'arrays!']
   wow = array[3]
 end
